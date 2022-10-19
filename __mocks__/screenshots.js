41235d9ea9d946e219de5fad4eae6f929ec234c9
DeadSkull786 <83582456+DeadSkull786@users.noreply.github.com>
2022-10-19 16:16:41 +0530

    Add screenshots

--- __mocks__/screenshots.js
+++ __mocks__/screenshots.js
@@ -16,4 +16,20 @@ export default [
     dateChecked: '2020-03-05T10:15:06Z',
     url: 'https://covidtracking.com/screenshot/3.png',
   },
+   {
+    date: '20200416',
+    dateChecked: '2020-03-05T10:15:06Z',
+    url: 'https://covidtracking.com/screenshot/3.png',
+  },
+   {
+    date: '20200516',
+    dateChecked: '2020-03-05T10:15:06Z',
+    url: 'https://covidtracking.com/screenshot/3.png',
+  },
+   {
+    date: '20200417',
+    dateChecked: '2020-04-17T10:15:06Z',
+    url: 'https://covidtracking.com/screenshot/2.png',
+  },
+
 ]
